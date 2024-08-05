9fa993b31bf58eaae1191d9faf746d5ce7da9852
Kitagawa19 <kkitagawa16219@gmail.com>
2024-08-05 23:03:15 +0000

    add page design

--- public/formtodbtest.php
+++ public/formtodbtest.php
@@ -37,27 +37,28 @@ $select_sth->execute();
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initail-scale=1.0">
     <title>掲示板</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
-<div>
-<div>
+<body>
+<div class="container">
+<div class="header">
 <h1>掲示板</h1>
 </div>
 <form method="POST" action="./formtodbtest.php">
-<textarea name="body"></textarea>
+<textarea name="body" placeholder="メッセージを入力してください"></textarea>
 <button type="submit">送信</button>
 </form>
-<div>
+<div class="pagination">
   <?= $page ?>ページ目
 (全<?= floor($count_all / $count_per_page) + 1 ?>ページ中)
   </div>
-  <div>
+  <div class="pagination">
   <?php if ($page > 1): ?>
   <a href="?page=<?= $page - 1 ?>">前へ</a>
   <?php endif; ?>
-  </div>
-  <div>
+  
   <?php if ($count_all > $page * $count_per_page): ?>
   <a href="?page=<?= $page + 1 ?>">次へ</a>
   <?php endif; ?>
@@ -70,15 +71,16 @@ $select_sth->execute();
       echo "このページは存在しません";
     } else {
       foreach ($select_sth as $data): ?>
-        <div>
-        <p><?= $data['id']?></p>
+        <div class="post">
+        <p class="post-id"><?= $data['id']?></p>
         </div>
-        <p>日時：<?= $data['created_at']?></p>
-        <p><?= nl2br(htmlspecialchars($data['content']))?></p>
+        <p class="post-date">日時：<?= $data['created_at']?></p>
+        <p class="post-content"><?= nl2br(htmlspecialchars($data['content']))?></p>
         <button type="submit">返信する</button>
         <?php endforeach; 
     }
   }
 ?>
 </div>
+</body>
 </html>
